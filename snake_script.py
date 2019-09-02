fb10a3493f22e20417b40cd5734f3e84d462fd83
Greco380 <greco.joshua@gmail.com>
2019-09-01 20:47:13 -0400

    Snake is running by itself. All I n eed to do is connect body to head

--- snake_script.py
+++ snake_script.py
@@ -50,47 +50,48 @@ class Snake(object):
     def move(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                keys = pygame.key.get_pressed()
+                pygame.quit()
+            keys = pygame.key.get_pressed()
 
-                for key in keys:
-                    if keys[pygame.K_LEFT]:
-                        self.dirnx = -1
-                        self.dirny = 0
-                        self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 
-                    elif keys[pygame.K_RIGHT]:
-                        self.dirnx = 1
-                        self.dirny = 0
-                        self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                elif keys[pygame.K_RIGHT]:
+                    self.dirnx = 1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 
-                    elif keys[pygame.K_UP]:
-                        self.dirnx = 0
-                        self.dirny = -1
-                        self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                elif keys[pygame.K_UP]:
+                    self.dirnx = 0
+                    self.dirny = -1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 
-                    elif keys[pygame.K_DOWN]:
-                        self.dirnx = 0
-                        self.dirny = 1
-                        self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                elif keys[pygame.K_DOWN]:
+                    self.dirnx = 0
+                    self.dirny = 1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 
-            for i, c in enumerate(self.body):
-                p = c.pos[:]
-                if p in self.turns:
-                    turn = self.turns[p]
-                    c.move(turn[0], turn[1])
-                    if i == len(self.body) - 1:
-                        self.turns.pop(p)
+        for i, c in enumerate(self.body):
+            p = c.pos[:]
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+                if i == len(self.body) - 1:
+                    self.turns.pop(p)
+            else:
+                if c.dirnx == -1 and c.pos[0] <= 0:
+                    c.pos = (c.rows - 1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows - 1:
+                    c.pos = (0, c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows - 1:
+                    c.pos = (c.pos[1], 0)
+                elif c.dirny == -1 and c.pos[1] <= 0:
+                    c.pos = (c.pos[0], c.rows - 1)
                 else:
-                    if c.dirnx == -1 and c.pos[0] <= 0:
-                        c.pos = (c.rows - 1, c.pos[1])
-                    elif c.dirnx == 1 and c.pos[0] >= c.rows - 1:
-                        c.pos = (0, c.pos[1])
-                    elif c.dirny == 1 and c.pos[1] >= c.rows - 1:
-                        c.pos = (c.pos[1], 0)
-                    elif c.dirny == -1 and c.pos[1] <= 0:
-                        c.pos = (c.pos[0], c.rows - 1)
-                    else:
-                        c.move(c.dirnx, c.dirny)
+                    c.move(c.dirnx, c.dirny)
 
     def reset(self, pos):
         self.head = cube(pos)
